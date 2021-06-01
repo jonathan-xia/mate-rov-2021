282526252e2c492d1b74d35da8ad9fff356cf7ce
marcuslee23 <77308446+marcuslee23@users.noreply.github.com>
2021-06-01 15:04:06 -0700

    Update thrusters_basic.ino

--- thrusters_basic.ino
+++ thrusters_basic.ino
@@ -19,14 +19,14 @@
      back_left.attach(39);
      mid_right.attach(43);
      mid_left.attach(37);
-     claw.attach(11);
+     claw.attach(12);
      front_right.writeMicroseconds(1500);
      front_left.writeMicroseconds(1500);
      back_right.writeMicroseconds(1500);
      back_left.writeMicroseconds(1500);
      mid_right.writeMicroseconds(1500);
      mid_left.writeMicroseconds(1500);
-     claw.writeMicroseconds(750);
+     claw.write(90);
      delay(7000);
      Serial.println("ready");
  }
@@ -36,9 +36,14 @@
     String input = Serial.readStringUntil('x');
     int dir = input.substring(0, 1).toInt();
     int thruster_value = input.substring(2, input.length() - 1).toFloat() * 230;
-    int claw_value = input.substring(2, input.length() - 1).toFloat()
+    int claw_value = input.substring(2, input.length() - 1).toFloat() * 85;
    
-    Serial.println("direction "+String(dir)+" thruster_value "+String(thruster_value));
+    if (dir == 2 or dir == 5){
+      Serial.println("direction " + String(dir) + " claw_value " + String(claw_value));
+    }
+    else{  
+      Serial.println("direction "+ String(dir) + " thruster_value "+String(thruster_value));
+    }
     //surge sway
     if (dir == 0){
     //  Serial.println("writing values to FRH, BRH, FLH, BLH");
@@ -66,9 +71,11 @@
       back_left.writeMicroseconds(thruster_value * -1 + 1500);
     }
     //claw open
-    else if (dir == 2){
-      claw.write(170);
-    }
+    //else if (dir == 2){
+    //  claw.write(90);
+      
+    //  claw.write(claw_value * -1 + 90);
+    //}
     //heave
     else if (dir == 3){
       mid_right.writeMicroseconds(1500);
@@ -89,9 +96,11 @@
       front_left.writeMicroseconds(thruster_value * -1 + 1500);
       back_left.writeMicroseconds(thruster_value * -1 + 1500);
     }
-    //claw close
+    //claw open and close
     else if (dir == 5){
       claw.write(90);
+      
+      claw.write(claw_value * -1 + 90);
     }
    }   
  }
